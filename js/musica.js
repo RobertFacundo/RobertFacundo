98382e6ea1737050fdcdd1faa7d937289d71d315
Facundo Robert <robert.facundo@hotmail.com>
2024-04-26 17:18:08 -0300

    tema light, modal de audio

--- js/musica.js
+++ js/musica.js
@@ -1,13 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let audioPlayer = document.getElementById("audioPlayer");
+// document.addEventListener("DOMContentLoaded", function() {
+//     let audioPlayer = document.getElementById("audioPlayer");
 
-    function toggleMute() {
-        if (audioPlayer.paused) {
-            audioPlayer.play(); // Iniciar la reproducción de música si está pausada
-        } else {
-            audioPlayer.pause(); // Pausar la reproducción de música si está reproduciéndose
-        }
-    }
+//     function toggleMute() {
+//         if (audioPlayer.paused) {
+//             audioPlayer.play(); // Iniciar la reproducción de música si está pausada
+//         } else {
+//             audioPlayer.pause(); // Pausar la reproducción de música si está reproduciéndose
+//         }
+//     }
 
-    document.getElementById("muteBtn").addEventListener("click", toggleMute);
-});
+//     document.getElementById("muteBtn").addEventListener("click", toggleMute);
+// });
