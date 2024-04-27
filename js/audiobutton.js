fd9ddc577d6078d787fc087e54b1630d9857c0cb
Facundo Robert <robert.facundo@hotmail.com>
2024-04-27 20:28:09 -0300

    comentario de secciones

--- js/audiobutton.js
+++ js/audiobutton.js
@@ -1,40 +1,40 @@
 
-document.addEventListener("DOMContentLoaded", function() {
-    let isMuted = false;
-    let modalShown = false;
-    let audioUrl = "../lcg_.wav"; 
+// document.addEventListener("DOMContentLoaded", function() {
+//     let isMuted = false;
+//     let modalShown = false;
+//     let audioUrl = "../lcg_.wav"; 
 
-    let audioPlayer = new Audio(audioUrl);
-    audioPlayer.load(); 
-    // let audioPlayer = document.getElementById("audioPlayer");
+//     let audioPlayer = new Audio(audioUrl);
+//     audioPlayer.load(); 
+//     // let audioPlayer = document.getElementById("audioPlayer");
 
-    function toggleMute() {
-        let btn = document.getElementById("muteBtn");
-        if (isMuted) {
-            btn.innerHTML = "&#x1D13D;"; 
-            btn.classList.remove("muted");
-            btn.classList.add("unmuted");
-            audioPlayer.play();
-        } else {
-            btn.innerHTML = "&#119136;"; 
-            btn.classList.remove("unmuted");
-            btn.classList.add("muted"); 
-            audioPlayer.pause(); 
-        }
-        isMuted = !isMuted;
-    }
+//     function toggleMute() {
+//         let btn = document.getElementById("muteBtn");
+//         if (isMuted) {
+//             btn.innerHTML = "&#x1D13D;"; 
+//             btn.classList.remove("muted");
+//             btn.classList.add("unmuted");
+//             audioPlayer.play();
+//         } else {
+//             btn.innerHTML = "&#119136;"; 
+//             btn.classList.remove("unmuted");
+//             btn.classList.add("muted"); 
+//             audioPlayer.pause(); 
+//         }
+//         isMuted = !isMuted;
+//     }
 
-    document.getElementById("muteBtn").addEventListener("click", function() {
-        if (!modalShown) {
-            document.getElementById("myModal").style.display = "flex";
-            modalShown = true;
-        } else {
-            toggleMute();
-        }
-    });
+//     document.getElementById("muteBtn").addEventListener("click", function() {
+//         if (!modalShown) {
+//             document.getElementById("myModal").style.display = "flex";
+//             modalShown = true;
+//         } else {
+//             toggleMute();
+//         }
+//     });
 
-    document.getElementById("confirmBtn").addEventListener("click", function() {
-        document.getElementById("myModal").style.display = "none";
-        toggleMute();
-    });
-});
+//     document.getElementById("confirmBtn").addEventListener("click", function() {
+//         document.getElementById("myModal").style.display = "none";
+//         toggleMute();
+//     });
+// });
